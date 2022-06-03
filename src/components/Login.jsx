f255e55ece2440fa6c0b7f366866d2df0d87941b
unknown <obirikan020@gmail.com>
2022-06-03 16:12:25 +0000

    + && -

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom'
 const Login = () => {
 const [name,setname]=useState('')
 const [password,setpass]=useState('')
+const [error,seterror]=useState()
 
 const send=async()=>{
     const config={
@@ -13,15 +14,18 @@ const send=async()=>{
             "Content-type":"application/json"
         }
     }
-    await axios.post('http://localhost:7000/api/users/login',{users:name,password:password},config).then(()=>{
+    await axios.post('http://localhost:7000/api/users/login',{users:name,password:password},config).then((res)=>{
+        console.log(res.data)
         setname('')
       }).catch((err)=>{
-          alert(err)
+          console.log(err)
+          seterror(err.response.data)
       })
   }
   return (
 <div>
     <h1>login</h1>
+    <h4 style={{color:'red'}}>{error}</h4>
     <input type="text" value={name} onChange={(e)=>setname(e.target.value)} />
     <br /><br />
     <input type="password" value={password} onChange={(e)=>setpass(e.target.value)} />
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -5,6 +5,7 @@ import axios from 'axios'
 const Register = () => {
  const [name,setname]=useState('')
  const [password,setpass]=useState('')
+ const [error,seterror]=useState()
 
  const send=async()=>{
    const config={
@@ -12,15 +13,18 @@ const Register = () => {
            "Content-type":"application/json"
        }
    }
-   await axios.post('http://localhost:7000/api/users/register',{users:name,password:password},config).then(()=>{
+   await axios.post('http://localhost:7000/api/users/register',{users:name,password:password},config).then((res)=>{
+     console.log(res.data)
      setname('')
-   }).catch((err)=>{
-       alert(err)
+     seterror('')
+   }).catch((error)=>{
+       seterror(error.response.data)
    })
  }
   return (
     <div>
         <h1>register</h1>
+        <h4 style={{color:'red'}}>{error}</h4>
         <input type="text" value={name} onChange={(e)=>setname(e.target.value)} />
         <br /><br />
         <input type="password" value={password} onChange={(e)=>setpass(e.target.value)} />
