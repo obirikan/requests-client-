4001570384786f23463906bb294000e3a2c5879d
unknown <obirikan020@gmail.com>
2022-06-05 00:02:59 +0000

    --&&++

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import Pages from "./components/Pages";
 import {BrowserRouter,Route} from 'react-router-dom'
+import Nav from "./components/Nav";
 
 function App() {
   return (
+  <div>
+
     <BrowserRouter>
        <Route path='/' component={Register} exact/>
        <Route path='/login' component={Login} exact/>
        <Route path='/main' component={Pages} exact/>
     </BrowserRouter>
+  </div>
   );
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div>
+       <Link to='/'><span>Sent request</span></Link>
+       <Link to='/'><span>received request</span></Link>
+    </div>
   )
 }
 
