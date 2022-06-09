952fa23e637cf35d76366e1d21e42647616cd918
unknown <obirikan020@gmail.com>
2022-06-09 10:16:23 +0000

    --&&++

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -3,37 +3,32 @@ import Nav from './Nav'
 import axios from 'axios'
 
 const Friends = () => {
-    const [data,setdata]=useState([])
-    const [user,setuser]=useState()
+    // const [data,setdata]=useState([])
+    // const [user,setuser]=useState()
 
-    useEffect(()=>{
-        const it=async()=>{
-        const user = JSON.parse(localStorage.getItem("info"));
-        const {person,token}=user
-        setuser(person)
-        const config = {
-          headers:{
-            Authorization: `Bearer ${user.token}`,
-               },
-        };
-           await axios.get('http://localhost:7000/api/handlers/friends',config).then((res)=>{
-          setdata(res.data)
-        }).catch((err)=>{
-            alert('network error')
-        })
-      }
-      it()
-    },[])
+    // useEffect(()=>{
+    //     const it=async()=>{
+    //     const user = JSON.parse(localStorage.getItem("info"));
+    //     const {person,token}=user
+    //     setuser(person)
+    //     const config = {
+    //       headers:{
+    //         Authorization: `Bearer ${user.token}`,
+    //            },
+    //     };
+    //        await axios.get('http://localhost:7000/api/handlers/friends',config).then((res)=>{
+    //       setdata(res.data)
+    //     }).catch((err)=>{
+    //         alert('network error')
+    //     })
+    //   }
+    //   it()
+    // },[])
   return (
     <div>
         <h1>Friendlist</h1>
         <><Nav/></>
-        <>
-        {data.map((a)=>(
-         <h3 key={a._id}>
-           {a.user}{user.friendlist.includes(a._id)&&<button>withdraw</button>}
-        </h3>
-       ))}</>
+        <>hey</>
     </div>
   )
 }
--- src/components/Pages.jsx
+++ src/components/Pages.jsx
@@ -4,16 +4,60 @@ import { Item } from '../context/Context'
 import Nav from './Nav'
 
 const Pages = () => {
-const [data,setdata]=useState([])
+let [data,setdata]=useState([])
+let [person,setperson]=useState()
+let [token,settoken]=useState()
 
+useEffect(()=>{
 
+   const it=async()=>{
+    const user= JSON.parse(localStorage.getItem('info'))
+    setperson(user)
+    settoken(user.token)
+    console.log(user)
+     const config={
+         headers:{
+           Authorization:`Bearer ${user.token || token }`
+         }
+     }
+    await axios.get('http://localhost:7000/api/handlers/allusers',config).then(res=>{
+      setdata(res.data)
+    }).catch((error)=>{
+      console.log(error)
+    })
+  }
+it()
+  
+},[])
+
+const send= async (pid)=>{
+  const user=JSON.parse(localStorage.getItem('info'))
+  const config={
+    headers:{
+      Authorization:`Bearer ${user.token}`
+    }
+}
+await axios.put('http://localhost:7000/api/handlers/send',{pid},config).then(res=>{
+ let data1=res.data
+ data1={...data1,token}
+ localStorage.setItem("info",JSON.stringify(data1))
+ setperson(data1)
+ settoken(data1.token)
+}).catch((error)=>{
+ console.log(error)
+})
+}
 
   return (
     <div>
       <h1>pages</h1>
       <Nav/>
       <>
-       
+        {data.map((all)=>(
+           <h3 key={all._id}>
+              {all.user}:{person.sendRequest.includes(all._id)?"pending":<button onClick={()=>{send(all._id)}}>send request</button>}
+            </h3>
+        ))}
       </>
     </div>
   )
