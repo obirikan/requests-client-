5f5d99309451e282c38732460fb42ed5081c8c92
unknown <obirikan020@gmail.com>
2022-06-09 16:20:08 +0000

    --&&++

--- src/components/Sent.jsx
+++ src/components/Sent.jsx
@@ -23,6 +23,26 @@ const Sent = () => {
       }
       it()
     },[])
+
+    const withdraw=async(id)=>{
+        const user=JSON.parse(localStorage.getItem('info'))
+        const token=user.token
+        const config={
+          headers:{
+            Authorization:`Bearer ${user.token}`
+          }
+      }
+       await axios.put('http://localhost:7000/api/handlers/unsend',{id},config).then(res=>{
+       let data1=res.data
+       data1={...data1,token}
+       localStorage.setItem("info",JSON.stringify(data1))
+       setuser(data1)
+    //    settoken(data1.token)})
+    }).catch((error)=>{
+       console.log(error)
+      })
+    }
+
   return (
     <div>
         <h1>sent request</h1>
@@ -30,7 +50,7 @@ const Sent = () => {
         <>
         {data.map((a)=>(
          <h3 key={a._id}>
-          {a.user}{user.sendRequest.includes(a._id)&&<button>withdraw</button>}
+          {a.user}{user.sendRequest.includes(a._id)?<button onClick={()=>{withdraw(a._id)}}>withdraw</button>:'removed'}
         </h3>
        ))}</>
     </div>
