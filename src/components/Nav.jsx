47fb4b45e72f1c941b68d8d2844e1b251f9ce632
unknown <obirikan020@gmail.com>
2022-06-14 17:00:39 +0000

    ++ css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <div style={{display:"grid",gridTemplateColumns:"1fr 1fr 1fr 1fr"}} className='nav'>
-       <Link to='/main' className='link'><p>home</p></Link>
-       <Link to='/sent' className='link'><p>Sent Request</p></Link>
-       <Link to='/friends'className='link'><p>Friendlist</p></Link>
-       <Link to='/recieve'className='link'><p>received Request</p></Link>
+       <Link to='/main' className='link'><p className='ic'>home</p><img className='imgg' src='imgs/home.png' alt='home'/></Link>
+       <Link to='/sent' className='link'><p className='ic'>Sent Request</p><img className='imgg' src='imgs/sent-2.png' alt='home'/></Link>
+       <Link to='/friends'className='link'><p className='ic'>Friendlist</p><img className='imgg' src='imgs/friends.png' alt='home'/></Link>
+       <Link to='/recieve'className='link'><p className='ic'>received Request</p><img className='imgg' src='imgs/recieved-2.png' alt='home'/></Link>
     </div>
   )
 }
